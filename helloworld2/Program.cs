0bc267049e08f6df70c517d16509166afdb1f804
sidpd51 <sidpd510@gmail.com>
2024-04-24 10:12:05 +0530

    add string

---
+++ helloworld2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string firstFriend = "Maria";
+string secondFriend = "Sage";
+string friends =    $"My friends are {firstFriend} and {secondFriend}";
+Console.WriteLine(friends);
+Console.WriteLine(friends.Replace("Maria", "Rahul"));
+Console.WriteLine(friends.Contains("Maria"));
+Console.WriteLine(friends.ToUpper());
+Console.WriteLine(friends.ToLower());
+Console.WriteLine(friends.StartsWith("Sco"));
+Console.WriteLine(friends.StartsWith("My  "));//significant white space(space inside string matter out doesn't matter)
+Console.WriteLine(friends.Length);
+Console.WriteLine(friends);
+
+
---
+++ number/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
